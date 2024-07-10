9d9a78de131f2c9ac9406355fd22e731de319553
Sp1rit <126536582+SFIRegret19@users.noreply.github.com>
2024-07-10 13:12:14 +0700

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,7 @@
+Number_of_completed_homework = 12
+Number_of_spent_hours = 1.5
+Name_course = 'Python'
+Time_for_one_task = Number_of_spent_hours / Number_of_completed_homework
+print('Курс:', Name_course, ', всего задач:', Number_of_completed_homework, ', затрачено часов:', Number_of_spent_hours, ', среднее время выполнения:', Time_for_one_task, 'часа.')
+#------------- or -------------
+print(f'Курс: {Name_course}, всего задач: {Number_of_completed_homework}, затрачено часов: {Number_of_spent_hours}, среднее время выполнения: {Time_for_one_task} часов.')
